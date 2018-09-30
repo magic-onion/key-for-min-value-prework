7198290acec5b7eca1a404fbebebb9959319548a
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-30 14:00:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   if name_hash.empty? === true 
     return nil 
   end
+  smallest = INFINITY
   name_hash.each do |key, value|
-    
+    if value < smallest
+      
   
 end
